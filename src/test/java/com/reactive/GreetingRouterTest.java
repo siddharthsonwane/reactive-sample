de87bc89a431f10a359cafbff9caaba496cc06ba
siddharth.sonwane <siddharthsonwane18@gmail.com>
2021-04-05 15:23:36 +0530

    Test case added

---
+++ src/test/java/com/reactive/GreetingRouterTest.java
@@ -0,0 +1,33 @@
+package com.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class GreetingRouterTest {
+
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	void contextLoads() {
+	}
+   @Test
+	public void testHello() {
+		webTestClient
+
+				.get().uri("/hello")
+				.accept(MediaType.TEXT_PLAIN)
+				.exchange()
+				.expectStatus().isOk()
+				.expectBody(String.class).isEqualTo("Hello, Spring!");
+	}
+
+}
